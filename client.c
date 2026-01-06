fd1999282c5844c810cf3c2747aabfc536452a41
Adrián Mrázik <mrazik2@stud.uniza.sk>
2026-01-06 18:05:57 +0100

    inicializacia novej hry

--- client.c
+++ client.c
@@ -6,7 +6,199 @@
 
 #define PORT 16767
 
+typedef struct {
+  int stavHry;
+} data_t;
+
+
+
+void* checkInput(void* arg) {
+
+  while (1) {
+    int input = getch();
+
+
+  }
+  return NULL;
+}
+
+int newGame(data_t* data) {  
+  char gameMode[2];
+  gameMode[1] = 0;
+  char worldType[2];
+  worldType[1] = 0;
+
+  char width[4];
+  width[3] = 0;
+  char height[4];
+  height[3] = 0;
+
+  char timeIn[4];
+  char fileName[30];
+
+  clear();
+  mvaddstr(1, 10, "Zvol herny rezim - standardny alebo casovy (s/c)");
+  while (1) {
+    int input = getch();
+    if (input == 's' || input == 'c') {
+      gameMode[0] = input;
+      break;
+    }
+  }
+  
+  clear();
+  mvaddstr(1, 10, "Zvol typ herneho sveta - bez prekazok alebo s prekazkami (b/s)");
+  while (1) {
+    int input = getch();
+    if (input == 'b' || input == 's') {
+      worldType[0] = input;
+      break;
+    }
+  }
+  
+  if (gameMode[0] == 'c') {
+    while (1) {
+      clear();
+      int input = 0;
+      mvaddstr(1, 10, "Zadaj cas hry v sekundach (max 999)");
+
+
+      getnstr(timeIn, 3);
+
+      while (1) {
+        clear();
+        mvaddstr(1, 10, "Napisal si:");
+        mvaddstr(2, 10, timeIn);
+        mvaddstr(4, 10, "Potvrd vyber (y/n)");
+
+        input = getch();
+      
+        if (input == 'y' || input == 'n') {
+          break;
+        }
+      }
+
+      if (input == 'y' && atoi(timeIn) > 0) {
+        break;
+      }
+    }
+  }
+
+  if (worldType[0] == 'b') {
+    while (1) {
+      clear();
+      int input = 0;
+      mvaddstr(1, 10, "Zadaj sirku sveta");
+
+
+      getnstr(width, 3);
+
+      while (1) {
+        clear();
+        mvaddstr(1, 10, "Napisal si:");
+        mvaddstr(2, 10, width);
+        mvaddstr(4, 10, "Potvrd vyber (y/n)");
+
+        input = getch();
+        
+        if (input == 'y' || input == 'n') {
+          break;
+        }
+      }
+
+      if (input == 'y' && atoi(width) > 0) {
+        break;
+      }
+    }
+
+    while (1) {
+      clear();
+      int input = 0;
+      mvaddstr(1, 10, "Zadaj vysku sveta");
+
+
+      getnstr(height, 3);
+
+      while (1) {
+        clear();
+        mvaddstr(1, 10, "Napisal si:");
+        mvaddstr(2, 10, height);
+        mvaddstr(4, 10, "Potvrd vyber (y/n)");
+
+        input = getch();
+        
+        if (input == 'y' || input == 'n') {
+          break;
+        }
+      }
+
+      if (input == 'y' && atoi(height) > 0) {
+        break;
+      }
+    }
+
+  }
+
+  if (worldType[0] == 's') {
+    while (1) {
+      clear();
+      int input = 0;
+      mvaddstr(1, 10, "Zadaj nazov suboru so svetom");
+
+
+      getnstr(fileName, 29);
+
+      while (1) {
+        clear();
+        mvaddstr(1, 10, "Napisal si:");
+        mvaddstr(2, 10, fileName);
+        mvaddstr(4, 10, "Potvrd vyber (y/n) alebo stlac q pre ukoncenie");
+
+        input = getch();
+        
+        if (input == 'q') {
+          return 0;
+        }
+        if (input == 'y' || input == 'n') {
+          break;
+        }
+      }
+
+      if (input == 'y') {
+        FILE *fptr = fopen(fileName, "r");
+        if (fptr != NULL) {
+          fclose(fptr);
+          break;
+        }
+      }
+    }
+
+  }
+
+  clear();
+  mvaddstr(1, 10, "Stlac hociake tlacidlo pre start");
+  getch();
+
+
+  const pid_t id = fork();
+  if (id < 0) {
+    perror("Fork zlyhal");
+    exit(-1);
+  } else if (id == 0) {
+    execlp("./server", "./server", gameMode, worldType, timeIn, width, height, fileName, NULL);
+    fprintf(stderr, "execlp failed\n");
+    exit(-1);
+  }
+
+  return 1;
+}
+
+
 int main(void) {
+  data_t data;
+  data.stavHry = 0;
+  
+  /*
   const pid_t id = fork();
   if (id < 0) {
     perror("Fork failed");
@@ -20,6 +212,8 @@ int main(void) {
   wait(NULL);
   printf("Hello from id: %d\n", id);
 
+  */
+
   initscr();
   keypad(stdscr, true);
   cbreak();
@@ -29,11 +223,14 @@ int main(void) {
   const char *menuItems[3] = {
   "Nova hra",
   "Pokracuj v hre",
-  "koniec"
+  "Koniec"
   };
 
   int selected = 0;
   int count = 3;
+
+  getch();
+  exit(-1);
 
 
   while(1) {
@@ -59,7 +256,11 @@ int main(void) {
     }
 
     if (input == 10) {
-      if (selected == 2) {
+      if (selected == 0) {
+        data.stavHry = newGame(&data);
+      } else if (selected == 1) {
+        continue;
+      } else {
         break;
       }
     }
--- server.c
+++ server.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char **argv) {
 
-    printf("Hello, hi, Server!\n");
+  
+  printf("Server arg count: %d\n", argc);
+  printf("mod hry: %s\n", argv[1]);
+  printf("typ sveta: %s\n", argv[2]);
+  printf("cas hry: %s\n", argv[3]);
+  printf("sirka pola: %s\n", argv[4]);
+  printf("vyska pola: %s\n", argv[5]);
+  printf("nazov suboru: %s\n", argv[6]);
 
-    return 0;
+  return 0;
 }
